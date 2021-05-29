a09114942c30e11d7d947dfaf53ea457bcf2129d
renatoexpert <renatorraraujo@gmail.com>
2021-05-29 20:38:02 -0300

    moon phase block

---
+++ script/second.js
@@ -0,0 +1,21 @@
+var showluz = document.getElementById('lumus');
+var sombra = document.getElementById('sombra');
+var mare = 0;
+
+var sombrapos = function () { return (ang/1.8)-25;};
+
+const sec_calc = function (alfa) {
+	let beta = Math.round(ang%180/1.8);
+	if (alfa >= 180) { return beta; mare=0 } 
+		else if (alfa < 180) { return 100-beta; mare=1 }
+		else {alert('error')}
+};
+
+const moon = function () {
+	var lumus = sec_calc(ang);
+	var mooncx = parseFloat(sombra.getAttributeNS(null, 'cx'));
+	showluz.innerHTML=lumus;
+	sombra.setAttributeNS(null, 'cx', sombrapos());
+	requestAnimationFrame (moon);
+}
+moon();
