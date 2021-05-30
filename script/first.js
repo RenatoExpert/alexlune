e16d909f98e9fea0794b0d7ed194fb7479fd7c89
renatoexpert <renatorraraujo@gmail.com>
2021-05-30 17:37:15 -0300

    ang become moonang, now we have moonang and terraang. Both are now based in a function of minutes

--- script/first.js
+++ script/first.js
@@ -1,20 +1,24 @@
 var posang = document.getElementById('posang');
 var lua1 = document.getElementById('lua1');
-var ang = 0;
+var terra = document.getElementById('terrain');
+var moonang = 0;
+var terraang = 0;
 
 const calcx = function (ang) { return ((Math.cos(toRadians(ang))*200)+260) };
 const calcy = function (ang) { return ((Math.sin(toRadians(ang))*200)+260) };
 
 function toRadians (angle) {return angle * (Math.PI / 180)}
+const get_angs = function () { terraang = Math.round((min/240)%360); moonang = Math.round((min/7200)%360) }  
 
 const ciclomaior = function () {
-	ang%=360;
-	posang.innerHTML = ang;
+	get_angs();
+	posang.innerHTML = moonang;
 	var lua1x = parseFloat(lua1.getAttributeNS(null, 'x'));
-	lua1.setAttributeNS(null, 'x', calcx(ang));
+	lua1.setAttributeNS(null, 'x', calcx(moonang));
 	var lua1y = parseFloat(lua1.getAttributeNS(null, 'y'));
-	lua1.setAttributeNS(null, 'y', calcy(ang));
-	ang++;
+	lua1.setAttributeNS(null, 'y', calcy(moonang));
+	terra.style.transform = 'rotate(' + terraang + 'deg)';
+	min+=3600;
 	requestAnimationFrame (ciclomaior);
 }
 ciclomaior();
