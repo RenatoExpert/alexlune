652034e3af1ec5a2d6a9171f550d4bd0908ae5fa
renatoexpert <renatorraraujo@gmail.com>
2021-06-19 22:24:55 -0300

    new global functions to calculate days,hours, and pure (min>60) minutes

--- script/main.js
+++ script/main.js
@@ -3,6 +3,10 @@ var min = 0;
 var stop = 1;
 var factor = 360000;
 
+const pureMin = function (mm) { return mm % 60};
+const hoursFromMin = function (mm) { return Math.round((mm/60)%24) };
+const daysFromMin = function (mm) { return Math.round(mm/1440) };
+
 const indispo = function () { window.alert('BRPT: Recurso ainda nao disponivel! \r\nEN: Not avaliable!')};
 const startAnimation = function () {
 	if ((stop == 0)) {stop = 1}
