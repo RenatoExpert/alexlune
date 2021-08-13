c243ae00ae317b59011ea8b8c39efdd99e03af9c
renatoexpert <renatorraraujo@gmail.com>
2021-08-13 10:38:26 -0300

    improving animation control. Now it changes its icon. Also stop is a boolean var and works inside animationController.onclick

--- script/main.js
+++ script/main.js
@@ -1,20 +1,31 @@
 var speed = document.getElementById('speed');
+var animationController = document.getElementById('animationController');
 var min = 0;
-var stop = 1;
 var factor = 360000;
 
 const pureMin = function (mm) { return mm % 60};
 const hoursFromMin = function (mm) { return Math.round((mm/60)%24) };
 const daysFromMin = function (mm) { return Math.round(mm/1440) };
+const changeTime = function (fac) { min += fac };
 
 const indispo = function () { window.alert('BRPT: Recurso ainda nao disponivel! \r\nEN: Not avaliable!')};
-const startAnimation = function () {
-	if ((stop == 0)) {stop = 1}
-	else {stop = 0};
-};
-const changeTime = function (fac) { min += fac };
+
+// Controller for animation (play/pause)
+animationController.innerHTML = '&#x23EF';
+var stop = true;
+animationController.onclick = function () {
+	stop=!stop;
+	if (stop == true) {
+		this.innerHTML = '&#x23EF';
+	}
+	else {
+		this.innerHTML = '&#x23F8';
+	}
+}
+
+// its the real animation wheel. Changes the min 
 const runcheck = function () { 
-	if ((stop == 0 )) { min += (speed.value*2) } 
+	if (stop == 0) { min += (speed.value*2) } 
 	requestAnimationFrame (runcheck)
 };
 runcheck();
