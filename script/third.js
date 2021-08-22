15749fb6aaa6d6bd49711d076ed18ee775f0b5cd
renatoexpert <renatorraraujo@gmail.com>
2021-08-22 18:08:47 -0300

    using calcs

--- script/third.js
+++ script/third.js
@@ -1,7 +1,7 @@
 TiDi = document.getElementById('localtime');
 
 const displayTime = function () {
-	var horario = new Date(Date.UTC(96,1,2,hoursFromMin(min)+18,pureMin(min) ));
+	var horario = new Date(Date.UTC(96, 1, 2, Calcs.hoursFromMin(min)+18, Calcs.pureMin(min) ));
 	TiDi.innerHTML = horario.getUTCHours() + ':' + horario.getUTCMinutes();
 	requestAnimationFrame(displayTime);
 }
