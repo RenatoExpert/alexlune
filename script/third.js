77514de0ab3127a558fd23d451f96f3d6c066d2f
renatoexpert <renatorraraujo@gmail.com>
2022-07-09 11:12:31 -0300

    using tolocalestring sv

--- script/third.js
+++ script/third.js
@@ -2,7 +2,7 @@ TiDi = document.getElementById('localtime');
 
 const displayTime = function () {
 	var horario = new Date(Date.UTC(96, 1, 2, Calcs.hoursFromMin(min)+18, Calcs.pureMin(min) ));
-	TiDi.innerHTML = horario.getUTCHours() + ':' + horario.getUTCMinutes();
+	TiDi.innerHTML = horario.toLocaleString('sv');
 	requestAnimationFrame(displayTime);
 }
 displayTime();
