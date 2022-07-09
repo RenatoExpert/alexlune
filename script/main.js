dde0680376813012702c1412c6ea6856c183ed80
renatoexpert <renatorraraujo@gmail.com>
2022-07-09 11:17:37 -0300

    sending third.js content to main.js

--- script/main.js
+++ script/main.js
@@ -103,3 +103,11 @@ const moon = function () {
 	lua.update();
 }
 moon();
+TiDi = document.getElementById('localtime');
+
+const displayTime = function () {
+	var horario = new Date(Date.UTC(96, 1, 2, Calcs.hoursFromMin(min)+18, Calcs.pureMin(min) ));
+	TiDi.innerHTML = horario.toLocaleString('sv');
+	requestAnimationFrame(displayTime);
+}
+displayTime();
