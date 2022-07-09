9ab67658c34d9e24dc96fc7be6fdff425548918c
renatoexpert <renatorraraujo@gmail.com>
2022-07-09 11:14:53 -0300

    sending astro to main

--- script/main.js
+++ script/main.js
@@ -39,3 +39,19 @@ class Calcs {
 const indispo = function () { window.alert('BRPT: Recurso ainda nao disponivel! \r\nEN: Not avaliable!')};
 
 Wheel.runcheck();
+class Astro {
+	constructor(id, time_factor) {
+		this.svg = document.getElementById(id);
+		this.time_factor = time_factor;
+	}
+	get angle () {
+		return Math.round((min/this.time_factor)%360);
+	}
+	update () {
+		var x = parseFloat(this.svg.getAttributeNS(null, 'x'));
+		var y = parseFloat(this.svg.getAttributeNS(null, 'y'));
+		this.svg.setAttributeNS(null,'x',Calcs.calc_x(this.angle));
+		this.svg.setAttributeNS(null,'y',Calcs.calc_y(this.angle));
+	}
+}
+
